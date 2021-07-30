3a6f6f76504bc240e7ba3507f6ea7fd5be548b4a
Eden AI <67952486+edenai@users.noreply.github.com>
2021-07-30 11:22:31 +0200

    Create edenai-keyword-extraction.php

---
+++ api_requests/edenai-keyword-extraction.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'HTTP/Request2.php';
+$request = new HTTP_Request2();
+$request->setUrl('https://api.edenai.run/v1/pretrained/text/keyword_extraction');
+$request->setMethod(HTTP_Request2::METHOD_POST);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'Authorization' => 'Bearer your_api_key'
+));
+$request->addPostParameter(array(
+  'text' => '\'I am angry today\'',
+  'providers' => '[\'microsoft\']',
+  'keywords_to_find' => 'angry',
+  'language' => 'en-US'
+));
+try {
+  $response = $request->send();
+  if ($response->getStatus() == 200) {
+    echo $response->getBody();
+  }
+  else {
+    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+    $response->getReasonPhrase();
+  }
+}
+catch(HTTP_Request2_Exception $e) {
+  echo 'Error: ' . $e->getMessage();
+}
