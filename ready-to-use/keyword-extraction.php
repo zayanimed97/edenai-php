60d45fabc3c91ce52eda221ca8e7b95321c45e86
Eden AI <67952486+edenai@users.noreply.github.com>
2021-09-09 15:56:31 +0200

    Eden AI with PHP: Keyword Extraction example
    Keyword extraction consists of automatically extracting the most important words and expressions in a text.

--- ready-to-use/keyword-extraction.php
+++ ready-to-use/keyword-extraction.php
@@ -8,6 +8,6 @@ use EdenAI\Text;
 $text = new Text("API KEY");
 
 # Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Keyword%20Extraction
-$output = $text->keywordExtraction(text:"Your text", provider: ['amazon', 'microsoft']);
+$output = $text->keywordExtraction(text:"Your text", provider: ['amazon', 'microsoft'], language: "en-US");
 echo $output;
 ?>
