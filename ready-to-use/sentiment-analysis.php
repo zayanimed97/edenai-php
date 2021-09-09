6c84e754c9a9b783b67d9296763c901e53272525
Eden AI <67952486+edenai@users.noreply.github.com>
2021-09-09 15:34:45 +0200

    Eden AI with Python: Sentiment analysis example
    Sentiment analysis extracts sentiment in a given string of text. Sentiment analysis, also called 'opinion mining', uses natural language processing, text analysis and computational linguistics to identify and detect subjective information from the input text.

---
+++ ready-to-use/sentiment-analysis.php
@@ -0,0 +1,13 @@
+<?php
+
+# Sentiment analysis extracts sentiment in a given string of text. Sentiment analysis, also called 'opinion mining', uses natural language processing, text analysis and computational linguistics to identify and detect subjective information from the input text.
+require_once './vendor/autoload.php';
+use EdenAI\Text;
+
+# Get your API key here: https://app.edenai.run/admin/account
+$text = new Text("API KEY");
+
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Sentiment%20Analysis
+$output = $text->sentimentAnalysis(text:"Your text", provider: ['amazon']);
+echo $output;
+?>
