6041b3d82c2b3b17f086fb395e1745f4131e62a1
Eden AI <67952486+edenai@users.noreply.github.com>
2021-09-09 15:49:09 +0200

    Eden AI with Python: Syntax analysis example
    Syntax analysis consists principaly in highlighting the structure of a text.

---
+++ ready-to-use/syntax-analysis.php
@@ -0,0 +1,13 @@
+<?php
+
+# Syntax analysis consists principaly in highlighting the structure of a text.
+require_once './vendor/autoload.php';
+use EdenAI\Text;
+
+# Get your API key here: https://app.edenai.run/admin/account
+$text = new Text("API KEY");
+
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Syntax%20Analysis
+$output = $text->sentimentAnalysis(text:"Your text", provider: ['amazon', 'google']);
+echo $output;
+?>
